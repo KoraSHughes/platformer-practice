fa27fc45bb47674a214e9cc3f46aaf8a78d3199f
Kora Hughes <korashughes@gmail.com>
2023-03-10 18:27:46 -0500

    Platform Mechanics/Movement

--- platformer/Assets/Scripts/Player.cs
+++ platformer/Assets/Scripts/Player.cs
@@ -4,21 +4,28 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public enum state{
+    public enum state{  // TODO: attach sprites to states
         idle,
         walking,
-            sprinting,
+                sprinting,
         jumping,
-            jump_sprinting,
+                jump_walking,
+                jump_sprinting,
             wall_jumping,
+                wall_jump_sprinting,
+                wall_jump_walking,
             double_jumping,  // half the height of jump & wall_jump
+                double_jump_walking,
+                double_jump_sprinting
     }
-    public state player_state = state.idle;
+    state player_state = state.idle;
     bool is_shooting = false;
 
-    public int walk_speed = 10;
-    public int sprint_speed = 10;
-    public int jump_height = 10;
+    public int walk_speed = 2;  // TODO: fix how the player moves with these
+    public int sprint_speed = 8;
+    public int jump_height = 5;
+    public int double_jump_height = 2;
+    // TODO: fix weird floating issue when player is in the air and moving
 
     GameManager _gameManager;
     Rigidbody2D _rigidbody2D;
@@ -39,19 +46,48 @@ public class Player : MonoBehaviour
         Debug.Log("Player State: " + player_state.ToString() + ", Shooting: " + is_shooting.ToString());
         float xVeloc = Input.GetAxis("Horizontal");
 
-        if (Input.GetAxis("Vertical") != 0){  // jumping
+        _rigidbody2D.angularVelocity = 0f; // TODO: make sure obj doesnt rotate
+
+        if (Input.GetButtonDown("Vertical")){  // jumping
             if (player_state == state.idle || player_state == state.walking || player_state == state.sprinting){
                 player_state = state.jumping;
-                _rigidbody2D.velocity = new Vector2(0, 10);
+                _rigidbody2D.velocity = new Vector2(0, jump_height);
+            }
+            else if (player_state == state.jumping && check_for_wall()){  // wall jumping
+                // TODO: add velocity perpendicular to wall
+                player_state = state.wall_jumping;
             }
             else if (player_state == state.jumping || player_state == state.jump_sprinting){ // double-jumping
                 player_state = state.double_jumping;
-                _rigidbody2D.velocity = new Vector2(0, 10);
+                _rigidbody2D.velocity = new Vector2(0, double_jump_height);
             }
         }
-        if (Input.GetAxis("Horizontal") != 0){ // movement
-            if(Input.GetButtonDown("Fire3")) { // sprinting
-                player_state = state.sprinting;
+
+        if (Input.GetAxis("Horizontal") == 0){
+            if (is_grounded()){
+                player_state = state.idle;
+            }
+        }
+        else { // movement
+            if(Input.GetButton("Fire3")) { // sprinting
+                if (player_state == state.jumping){
+                    player_state = state.jump_sprinting;
+                }
+                else if (player_state == state.jump_walking){
+                    player_state = state.jump_sprinting;
+                }
+                else if (player_state == state.wall_jumping){
+                    player_state = state.wall_jump_sprinting;
+                }
+                else if (player_state == state.double_jumping){
+                    player_state = state.double_jump_sprinting;
+                }
+                else if (player_state == state.idle){
+                    player_state = state.sprinting;
+                }
+                else if (player_state == state.walking){
+                    player_state = state.sprinting;
+                }
                 _rigidbody2D.velocity = new Vector2(xVeloc*sprint_speed, 0);
             }
             else{  // walking
@@ -63,5 +99,21 @@ public class Player : MonoBehaviour
         if (Input.GetButton("Jump")){ // shooting
             is_shooting = true;
         }
+        else{
+            is_shooting = false;
+        }
+    }
+
+    private bool check_for_wall(){
+        // TODO: check for nearby wall
+        return false;
+    }
+    private bool is_grounded(){
+        // check if player on platform or not moving?
+        return _rigidbody2D.velocity == Vector2.zero;
+    }
+
+    public state get_state(){
+        return player_state;
     }
 }
