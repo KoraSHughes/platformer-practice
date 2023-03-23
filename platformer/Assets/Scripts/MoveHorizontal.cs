3a3097933eea4763317c663797c72765a8daae47
Nishma <ns4606@nyu.edu>
2023-03-22 23:42:55 -0400

    4 levels, scripts to move
    colors:
    red = danger/kill zone
    yellow = moving parts
    blue= player

---
+++ platformer/Assets/Scripts/MoveHorizontal.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveHorizontal : MonoBehaviour
+{
+    private Vector2 startPos;
+    private Vector2 newPos;
+    public float speed = 0.5f;// SPEED OF MOVEMENT
+    public int distance = 1; // MAX DISTANCE FOR DIRECTION X
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+        newPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        newPos.x = startPos.x + (distance * Mathf.Sin(Time.time * speed));
+        transform.position = newPos;
+    }
+}
---
+++ platformer/Assets/Scripts/MoveVertical.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveVertical : MonoBehaviour
+{
+    private Vector2 startPos;
+    private Vector2 newPos;
+    public float speed = 1;// SPEED OF MOVEMENT
+    public int distance = 5; // MAX DISTANCE FOR DIRECTION Y
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+        newPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        newPos.y = startPos.y + (distance * Mathf.Sin(Time.time * speed));
+        transform.position = newPos;
+    }
+}
