e616fac29f2e8e4be70d3514ccaecbc720db0c1b
Kora Hughes <korashughes@gmail.com>
2023-03-24 17:33:03 -0400

    Movement Overhaul
    ~better for state definition
    ~added jump and dash code

--- platformer/Assets/Scripts/Player.cs
+++ platformer/Assets/Scripts/Player.cs
@@ -6,27 +6,24 @@ public class Player : MonoBehaviour
 {
     public int default_double_jumps = 1;
     public int default_wall_jumps = 1;
+    public int default_dashes = 1;
+    float jump_cooldown = 0f;
+    float dash_cooldown = 0f;
+    float dash_dist = 7f;
     float walk_speed = 5;
-    float sprint_speed;
     float jump_height = 4f;
-    float double_jump_height;
+    float double_jump_height = 3f;
 
     public enum state{  // TODO: attach sprites to states
         idle,
             // attacking,
         walking,
             // walk_attacking,
-                sprinting,
-                    // sprint_attacking,
         jumping,  // Note: anything past this counts as jumping (index: 3)
                 jump_walking,
-                jump_sprinting,
-            wall_jumping,
-            //     wall_jump_sprinting,
-            //     wall_jump_walking
-            double_jumping
-            //     double_jump_walking,
-            //     double_jump_sprinting
+            dashing,
+            double_jumping,
+            wall_jumping
     }
     state player_state = state.idle;
     bool is_attacking = false;
@@ -44,10 +41,9 @@ public class Player : MonoBehaviour
     {
         _gameManager = GameObject.FindObjectOfType<GameManager>();
         _rigidbody2D = GetComponent<Rigidbody2D>();
+
         double_jumps = default_double_jumps;
         wall_jumps = default_wall_jumps;
-        updateSprint();
-        updateJump();
     }
 
     void FixedUpdate()
@@ -58,37 +54,77 @@ public class Player : MonoBehaviour
     void Update()
     {
         is_grounded = Physics2D.OverlapCircle(feet.transform.position, .3f, whatIsGround);
-
-
-        Debug.Log("Player State: " + player_state.ToString() + ", Jumps: "+ (double_jumps, wall_jumps).ToString()
-                  + ", Attacking: " + is_attacking.ToString() + ", Grounded|Jumping|Still?" + (is_grounded, is_jumping(), is_still()).ToString());
-        if (is_grounded && is_jumping()){
-            Debug.LogError("Contradiction in Jumping & Groundedness");
+        if (dash_cooldown > 0){
+            dash_cooldown -= Time.deltaTime;
+        }
+        else{
+            dash_cooldown = 0f;  // saftey in case time subtracts more than necessary
+        }
+        if (jump_cooldown > 0){
+            jump_cooldown -= Time.deltaTime;
+        }
+        else{
+            jump_cooldown = 0;
         }
 
         _rigidbody2D.angularVelocity = 0f; // TODO: make sure obj doesnt rotate
 
 
-        if (is_grounded){  // double jump reset
+        Debug.Log("Player State: " + player_state.ToString() + ", Jumps: "+ (double_jumps, wall_jumps).ToString()
+                  + ", Attacking: " + is_attacking.ToString() + ", Grounded|Still?" + (is_grounded, is_still()).ToString()
+                  + ", Dash CD: " + dash_cooldown.ToString() + ", Jump CD: " + jump_cooldown.ToString());
+
+        float yMove = Input.GetAxis("Vertical");
+        float xMove = Input.GetAxis("Horizontal");
+
+        if (is_grounded){  // on the ground
             double_jumps = default_double_jumps;
             wall_jumps = default_wall_jumps;
 
-            if (is_still()){
-                player_state = state.idle; // player idle
+            if(Input.GetButton("Fire3") && dash_cooldown == 0){  // dashing
+                int facing = (xMove >= 0) ? 1 : -1;
+                _rigidbody2D.velocity = new Vector2(dash_dist*facing, _rigidbody2D.velocity.y/2);
+                //Note: we half our vertical velocity (better feel)
+                dash_cooldown += 2f;
+                player_state = state.dashing;
+            }
+            else if (yMove > 0 && jump_cooldown == 0) { // jumping
+                _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, jump_height);
+                jump_cooldown += 0.7f;
+                player_state = state.jumping;
+            }
+            else if (xMove != 0){  // walking
+                _rigidbody2D.velocity = new Vector2(xMove*walk_speed, _rigidbody2D.velocity.y);
+                player_state = state.walking;
+            }
+            else{
+                if (is_still()){
+                    player_state = state.idle; // player idle
+                }
+                else{
+                    player_state = state.walking;
+                }
             }
         }
-        else{
-            // player_state.state = state.jumping;
-        }
+        else{  // mid-air
+            if(Input.GetButton("Fire3") && dash_cooldown == 0){  // dashing
+                int facing = (xMove >= 0) ? 1 : -1;
+                _rigidbody2D.velocity = new Vector2(dash_dist*facing, _rigidbody2D.velocity.y/2);
+                //Note: we half our vertical velocity (better feel)
+                dash_cooldown += 2f;
+                player_state = state.dashing;
+            }
+            else if (yMove > 0 && jump_cooldown == 0) { // extra jumping
+                _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, jump_height);
+                jump_cooldown += 0.5f;
+                player_state = state.jumping;
 
-        if (Input.GetButtonDown("Vertical")){  // jumping attempt TODO: change to button?
-            if (is_jumping()){
                 int wall_dir = check_for_wall();
                 if (wall_jumps > 0 && wall_dir != 0){  // wall-jumping
-                    _rigidbody2D.velocity = new Vector2(wall_dir*sprint_speed, double_jump_height);
+                    _rigidbody2D.velocity = new Vector2(wall_dir*walk_speed, double_jump_height);
                     player_state = state.wall_jumping;
                     wall_jumps -= 1;
-                    double_jumps += 1;
+                    // double_jumps += 1;
                 }
                 else if(double_jumps > 0){   // double-jumping
                     if (_rigidbody2D.velocity.y > double_jump_height){
@@ -100,40 +136,21 @@ public class Player : MonoBehaviour
                     player_state = state.double_jumping;
                     double_jumps -= 1;
                 }
-            }
-            else if (is_grounded){
-                _rigidbody2D.velocity += new Vector2(0, jump_height);
-                player_state = state.jumping;
-                // TODO: fix state where jump height is lower when moving
-                // TODO: fix state where double jump counter decrements from normal jump
-            }
-        }
-
-
-        float xMove = Input.GetAxis("Horizontal");
-        if (xMove != 0){  // horizontal movement attempt
-            if(Input.GetButton("Fire3")) {  // sprinting
-                if (is_jumping()){
-                    player_state = state.jump_sprinting;
-                }
                 else{
-                    player_state = state.sprinting;
+                    player_state = state.jumping;
                 }
-                _rigidbody2D.velocity = new Vector2(xMove*sprint_speed, _rigidbody2D.velocity.y);
             }
-            else{  // walking
-                if (is_jumping()){
-                    player_state = state.jump_walking;
-                }
-                else{
-                    player_state = state.walking;
-                }
+            else if (xMove != 0){  // walking
                 _rigidbody2D.velocity = new Vector2(xMove*walk_speed, _rigidbody2D.velocity.y);
+                player_state = state.jump_walking;
+            }
+            else{
+                player_state = state.jumping;
             }
         }
-
-
+        
         if (Input.GetButton("Jump")){ // attacking
+            //TODO: maybe add a attacking cooldown
             is_attacking = true;
         }
         else{
@@ -145,12 +162,8 @@ public class Player : MonoBehaviour
         // TODO: finds nearest wall and returns -1 if left of user, 0 if not close enough, and 1 if right of user
         return 0;
     }
-    private bool is_jumping(){  // we are jumping
-        // jumping/double-jumping/wall-jumping + any combo of sprinting/walking/attacking
-        return (int)player_state >= (int)state.jumping;
-    }
     private bool is_still(){  // not moving much horizontally
-        return _rigidbody2D.velocity.x <= 0.5f && _rigidbody2D.velocity.x >= -0.5f;
+        return _rigidbody2D.velocity.x <= 0.5f && _rigidbody2D.velocity.x >= -0.5f && _rigidbody2D.velocity.y <= 0.3f && _rigidbody2D.velocity.y >= -0.3f;
     }
     
 
@@ -163,11 +176,5 @@ public class Player : MonoBehaviour
         double_jumps += doubles;
         wall_jumps += walls;
         return (double_jumps, wall_jumps);
-    }
-    public void updateSprint(){
-        sprint_speed = walk_speed*1.5f;
-    }
-    public void updateJump(){
-        double_jump_height = jump_height/1.5f;
     }
 }
