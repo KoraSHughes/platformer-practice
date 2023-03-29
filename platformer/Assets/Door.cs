0c4b4cb5d9177cf0e71b5566302494d10db3657b
Nishma <ns4606@nyu.edu>
2023-03-29 01:11:06 -0400

    levels update
    added door prefab
    door.cs script
    edits to game manager script

---
+++ platformer/Assets/Door.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Door : MonoBehaviour
+{
+
+    int ranInt;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        // if player collides with door, load next room?
+        if (other.CompareTag("Player")) {
+            Debug.Log("collision");
+            // SceneManager.LoadScene(1);
+            ranInt = SceneManager.GetActiveScene().buildIndex + 1;
+            SceneManager.LoadScene(ranInt);
+        }
+    }
+
+
+}
--- platformer/Assets/Scripts/GameManager.cs
+++ platformer/Assets/Scripts/GameManager.cs
@@ -6,10 +6,25 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    // check scene build index... 
+    // display corresponding info (tutorial) for the abilities player has
+
+    public TextMeshProUGUI levelText;
+    string walkJump;
+    // string dash = "Press A and SHIFT or left and SHIFT to double jump";
+    // string doubleJump = "Press SPACE while in the air to double jump";
+    // string wallJump = "When jumping on a wall, press SPACE to jump over it";
+    // string attack = "Press the Left Mouse Button to attack the enemy";
+
+    int buildIndex;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        walkJump = "Press A to go left, Press D to go right, Press W to jump";
+        buildIndex = SceneManager.GetActiveScene().buildIndex;
+        // call coroutine
     }
 
     // Update is called once per frame
@@ -24,5 +39,26 @@ public class GameManager : MonoBehaviour
 
     public void QuitGame() {
         Application.Quit();
+    }
+
+    public IEnumerator ShowTutorial() {
+        // if (buildIndex == 1) {
+        //     levelText.text = walkJump;
+        // }
+        // if (buildIndex == 2) {
+        //     levelText.text = dash;
+        // }
+        // if (buildIndex == 3) {
+        //     levelText.text = doubleJump;
+        // }
+        // if (buildIndex == 4) {
+        //     levelText.text = wallJump;
+        // }
+        // if (buildIndex == 5) {
+        //     levelText.text = attack;
+        // }
+        levelText.text = walkJump;
+        yield return new WaitForSeconds(3f);
+        levelText.text = "";
     }
 }
